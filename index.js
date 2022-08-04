5b63aa3f5749036c9868ea835641533bdaf46959
dennismuturi1@gmail.com <dennis@WIN-AVPLU2QPEMC.localdomain>
2022-08-04 16:44:48 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+
+const main_element = document.getElementById('main');
+main_element.remove();
+
+const newHeader=document.createElement('h1');
+newHeader.setAttribute('id','victory');
+newHeader.innerHTML="Dennis is the champion";
+newHeader.append(main);
